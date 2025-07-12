6f86b13fe6af92f2620eb9337bdb193033463d6f
ghazafm <fauzan.ghaza@gmail.com>
2025-07-13 00:58:34 +0700

    fix(logging): save

--- simulation/train.py
+++ simulation/train.py
@@ -8,12 +8,13 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 env = K8sAutoscalerEnv(
     deployment_name="nginx-deployment",
-    iteration=3,
+    iteration=100,
     verbose=True,
     action_step=50,
     timeout=30,
 )
-model = DQN(policy="MlpPolicy", env=env, verbose=1, buffer_size=10000)
+log_path = Path("training") / "logs"
+model = DQN(policy="MlpPolicy", env=env, verbose=1, tensorboard_log=str(log_path))
 model.learn(total_timesteps=10000)
 simulation_path = Path("training") / "model" / "Shower_Model_DQN"
 model.save(simulation_path)
